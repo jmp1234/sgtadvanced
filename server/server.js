3e4ed71e837f3bd1bd328c70c885f4b7934c4553
jmp1234 <johnnypham944@yahoo.com>
2019-03-22 13:07:35 -0700

    added get data endpoint in full

--- server/server.js
+++ server/server.js
@@ -2,37 +2,51 @@
 
 const express = require('express'); //load the express library into the file
 const mysql = require('mysql');
-const mysqlcredentials = require('./mysqlcreds.js');
-
+const mysqlcredentials = require('./mysqlcreds.js');//load the credentials from a local file for mysql
+//using the credentials that we loaded, establish a preliminary connection to the database
 const db = mysql.createConnection( mysqlcredentials )
 
 const server = express(); //you now have a server! express is running node
 
 server.use( express.static( __dirname + '/html' ) ); //middleware //looks in the html folder for a file called index.html, and grabs that file and serves it
+server.use( express.urlencoded( { extended: false } )) //have express pull body data that is urlencoded and place it into an object called body
+
 
 //ex: in the url, I can add /components/students.js to read that file on the browser
 
+//takes a callback function once the connection (request) is made at port 3001 at the specified location (/api/grades)
+//make an enpoint to handle retrieving the grades of all students
 server.get( '/api/grades', ( req, res ) => {
-  res.send(`{
-  	"success": true,
-  	"data": [{
-  		"id": 10,
-  		"name": "Johnny",
-  		"course": "math",
-  		"grade": 66
-  	}, {
-  		"id": 12,
-  		"name": "Bob",
-  		"course": "science",
-  		"grade": 83
-  	}, {
-  		"id": 14,
-  		"name": "John",
-  		"course": "english",
-  		"grade": 98
-  	}]
-  }`)
+  //establish the connection to the database, and call the callback function when connection is made
+  db.connect( () => {
+    //create a query for our desired operation
+    const query = 'SELECT `id`, CONCAT(`givenname`, " ", `surname`) AS `name`, `course`, `grade` FROM `grades`';
+    //send the query to the database, and call the given callback function once the data is retreived or an error happens
+    db.query( query, ( error, data ) => { //send query to database
+      //if error is null, no error occured
+      //create an output object to be sent back to the client
+      const output = {
+        success: false,
+      }
+      //if error is null, send the data
+      if(!error) {
+        //notify the client that we were successful
+        output.success = true;
+        //attach the data from the database in the output object
+        output.data = data;
+      } else {
+        //an error occurred, attach that error onto the output so we can see what happened
+        output.error = error
+      }
+      //send the data back to the client
+      res.send( output );
+    }); //call the function when sent
+  }) //establish the db connection if that address is requested
 }) //the api name doesn't matter - it can be named anything
+
+server.post( '/api/grades', ( request, response ) => {
+
+})
 
 server.listen( 3001, () => { //port and callback function
   console.log('server is running on port 3001');
